d2faf400df3856a2ab726cfa7e77e9f3af12e1a1
Valeriia1997 <seliverstova.valerya@gmail.com>
2024-10-29 02:25:56 +0200

    feat: hw-07

---
+++ goit-algo-hw-07/task1.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+def max_value_node(node):
+    current = node
+    while current.right:
+        current = current.right
+    return current
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+# Test
+root = Node(5)
+root = insert(root, 1)
+root = insert(root, 23)
+root = insert(root, 42)
+root = insert(root, 74)
+root = insert(root, 87)
+root = insert(root, 26)
+root = insert(root, 8)
+
+print(max_value_node(root))  # 87
+
